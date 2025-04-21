d505f9a685d4e8b96d4bc6222062bc79998ce02f
Chris Ramón <rchristian.ramon@gmail.com>
2025-04-21 15:52:23 -0500

    domain/solutions: adds initial scaffolding

---
+++ domain/solutions/mappers/mappers.go
@@ -0,0 +1,16 @@
+package mappers
+
+import (
+	"github.com/chris-ramon/golang-scaffolding/domain/solutions/api"
+	"github.com/chris-ramon/golang-scaffolding/domain/solutions/types"
+)
+
+func SolutionsFromTypeToAPI(solutions []*types.Solution) []api.Solution {
+	result := []api.Solution{}
+
+	// for _, solution := range solutions {
+	// 	result = append(result, api.Solution{})
+	// }
+
+	return result
+}
