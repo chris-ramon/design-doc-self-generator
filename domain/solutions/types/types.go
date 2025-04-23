3096a54dbc8b79c24603c5cbbb88746d8daea2f1
Chris Ramón <rchristian.ramon@gmail.com>
2025-04-23 16:17:51 -0500

    domain/solutions: adds solution related internal types

--- domain/solutions/types/types.go
+++ domain/solutions/types/types.go
@@ -1,4 +1,40 @@
 package types
 
 type Solution struct {
+	AnalysisSet AnalysisSet
+}
+
+type SolutionSet []Solution
+
+type AnalysisSet []Analysis
+
+type Analysis struct {
+	InformationSet InformationSet
+}
+
+type InformationSet []Information
+
+type Information struct {
+	GitHub GitHub
+}
+
+type GitHub struct {
+	Metrics Metrics
+}
+
+type Metrics struct {
+	PullRequests PullRequests
+}
+
+type PullRequests []PullRequest
+
+type PullRequest struct {
+	Duration     int
+	Contributors Contributors
+}
+
+type Contributors []Contributor
+
+type Contributor struct {
+	ProfileUrl string
 }
