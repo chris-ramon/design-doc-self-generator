30c1ac4ea82cc3fc1d0bda410e86b6a9ef6f66fa
Chris Ramón <rchristian.ramon@gmail.com>
2025-04-23 16:21:39 -0500

    domain/solutions: adds solutions internal to external related mappers

--- domain/solutions/mappers/mappers.go
+++ domain/solutions/mappers/mappers.go
@@ -5,12 +5,99 @@ import (
 	"github.com/chris-ramon/golang-scaffolding/domain/solutions/types"
 )
 
-func SolutionsFromTypeToAPI(solutions []*types.Solution) []api.Solution {
-	result := []api.Solution{}
+func SolutionsFromTypeToAPI(solutions types.SolutionSet) api.SolutionSet {
+	result := api.SolutionSet{}
 
-	// for _, solution := range solutions {
-	// 	result = append(result, api.Solution{})
-	// }
+	for _, s := range solutions {
+		solution := solutionFromTypeToAPI(s)
+		result = append(result, solution)
+	}
+
+	return result
+}
+
+func solutionFromTypeToAPI(solution types.Solution) api.Solution {
+	result := api.Solution{}
+
+	for _, a := range solution.AnalysisSet {
+		analysis := analysisFromTypeToAPI(a)
+		result.AnalysisSet = append(result.AnalysisSet, analysis)
+	}
+
+	return result
+}
+
+func analysisFromTypeToAPI(analysis types.Analysis) api.Analysis {
+	result := api.Analysis{}
+
+	for _, i := range analysis.InformationSet {
+		information := informationFromTypeToAPI(i)
+		result.InformationSet = append(result.InformationSet, information)
+	}
+
+	return result
+}
+
+func informationFromTypeToAPI(information types.Information) api.Information {
+	result := api.Information{
+		GitHub: githubFromTypeToAPI(information.GitHub),
+	}
+
+	return result
+}
+
+func githubFromTypeToAPI(github types.GitHub) api.GitHub {
+	result := api.GitHub{
+		Metrics: metricsFromTypeToAPI(github.Metrics),
+	}
+
+	return result
+}
+
+func metricsFromTypeToAPI(metrics types.Metrics) api.Metrics {
+	result := api.Metrics{
+		PullRequests: pullRequestsFromTypeToAPI(metrics.PullRequests),
+	}
+
+	return result
+}
+
+func pullRequestsFromTypeToAPI(pullRequests types.PullRequests) api.PullRequests {
+	result := api.PullRequests{}
+
+	for _, pr := range pullRequests {
+		pullRequest := pullRequestFromTypeToAPI(pr)
+
+		result = append(result, pullRequest)
+	}
+
+	return result
+}
+
+func pullRequestFromTypeToAPI(pullRequest types.PullRequest) api.PullRequest {
+	result := api.PullRequest{
+		Duration:     pullRequest.Duration,
+		Contributors: contributorsFromTypeToAPI(pullRequest.Contributors),
+	}
+
+	return result
+}
+
+func contributorsFromTypeToAPI(contributors types.Contributors) api.Contributors {
+	result := api.Contributors{}
+
+	for _, c := range contributors {
+		contributor := contributorFromTypeToAPI(c)
+		result = append(result, contributor)
+	}
+
+	return result
+}
+
+func contributorFromTypeToAPI(contributor types.Contributor) api.Contributor {
+	result := api.Contributor{
+		ProfileUrl: contributor.ProfileUrl,
+	}
 
 	return result
 }
