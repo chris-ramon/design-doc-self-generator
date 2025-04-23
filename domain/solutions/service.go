ba6176a569ea27482b066404179fac756340b95d
Chris Ramón <rchristian.ramon@gmail.com>
2025-04-23 16:31:27 -0500

    domain/solutions: adds SolutionService.FindAnalysis related test data

--- domain/solutions/service.go
+++ domain/solutions/service.go
@@ -6,11 +6,38 @@ import (
 	"github.com/chris-ramon/golang-scaffolding/domain/solutions/types"
 )
 
+var testData types.SolutionSet = types.SolutionSet{
+	types.Solution{
+		AnalysisSet: types.AnalysisSet{
+			types.Analysis{
+				InformationSet: types.InformationSet{
+					types.Information{
+						GitHub: types.GitHub{
+							Metrics: types.Metrics{
+								PullRequests: types.PullRequests{
+									types.PullRequest{
+										Duration: 1,
+										Contributors: types.Contributors{
+											types.Contributor{
+												ProfileUrl: "profileUrl",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
 type service struct {
 }
 
-func (s *service) FindAnalysis(ctx context.Context) ([]*types.Solution, error) {
-	result := []*types.Solution{}
+func (s *service) FindAnalysis(ctx context.Context) (types.SolutionSet, error) {
+	result := testData
 
 	return result, nil
 }
