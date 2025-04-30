1844c2638f89c56373aa90637b57b4fa4f21fe9a
Chris Ramón <rchristian.ramon@gmail.com>
2025-04-30 14:57:10 -0500

    domain: syncs metrics.FindPullRequests method

--- domain/metrics/service.go
+++ domain/metrics/service.go
@@ -2,7 +2,6 @@ package metrics
 
 import (
 	"context"
-	"log"
 	"net/http"
 
 	"github.com/chris-ramon/golang-scaffolding/domain/metrics/types"
@@ -15,42 +14,50 @@ type service struct {
 }
 
 type FindPullRequestsResult struct {
+	PullRequests []*types.PullRequest
+}
+
+type findPullRequestsResult struct {
 	PullRequest *types.PullRequest
 }
 
-func (s *service) FindPullRequests(ctx context.Context, params types.FindPullRequestsParams) (string, error) {
+func (s *service) FindPullRequests(ctx context.Context, params types.FindPullRequestsParams) (*FindPullRequestsResult, error) {
+	result := &FindPullRequestsResult{}
+
 	for _, pr := range params {
-		s.findPullRequests(ctx, pr)
+		r, err := s.findPullRequests(ctx, pr)
+		if err != nil {
+			return nil, err
+		}
+
+		result.PullRequests = append(result.PullRequests, r.PullRequest)
 	}
 
-	return "ok", nil
+	return result, nil
 }
 
-func (s *service) findPullRequests(ctx context.Context, param types.FindPullRequestParam) (string, error) {
+func (s *service) findPullRequests(ctx context.Context, param types.FindPullRequestParam) (*findPullRequestsResult, error) {
 	// Create a GitHub client using the provided HTTP client.
 	client := github.NewClient(s.HTTPClient)
 
 	// Fetch pull request information from GitHub.
 	pullRequest, _, err := client.PullRequests.Get(ctx, param.Owner, param.Repo, param.Number)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	// Extract pull request metrics.
-	duration := pullRequest.CreatedAt.Sub(*pullRequest.MergedAt)
+	duration := pullRequest.MergedAt.Sub(*pullRequest.CreatedAt)
 	pr := &types.PullRequest{
 		Duration: duration,
 	}
 
 	// Create the result.
-	result := &FindPullRequestsResult{
+	result := &findPullRequestsResult{
 		PullRequest: pr,
 	}
 
-	log.Printf("result: %+v", result)
-
-	return "ok", nil
-
+	return result, nil
 }
 
 func NewService(HTTPClient *http.Client) (*service, error) {
