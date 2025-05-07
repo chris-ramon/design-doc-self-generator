dce7688df5b27a23fb3b44dd7f0bc111edacb356
Chris Ramón <rchristian.ramon@gmail.com>
2025-05-07 16:54:23 -0500

    domain/metrics: wires PullRequest.FormattedContributors field

--- domain/metrics/service.go
+++ domain/metrics/service.go
@@ -94,11 +94,12 @@ func (s *service) findPullRequests(ctx context.Context, param types.FindPullRequ
 	// Extract pull request metrics.
 	duration := pullRequest.MergedAt.Sub(*pullRequest.CreatedAt)
 	pr := &types.PullRequest{
-		Duration:     duration,
-		CreatedAt:    pullRequest.CreatedAt,
-		MergedAt:     pullRequest.MergedAt,
-		URL:          param.URL,
-		Contributors: contributors,
+		Duration:              duration,
+		CreatedAt:             pullRequest.CreatedAt,
+		MergedAt:              pullRequest.MergedAt,
+		URL:                   param.URL,
+		Contributors:          contributors,
+		FormattedContributors: contributors.FormattedContributors(),
 	}
 
 	// Create the result.
