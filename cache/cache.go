a6c53b2f439e2fc79fe6439fd0736629e23f1f2c
Chris Ramón <rchristian.ramon@gmail.com>
2025-07-31 09:07:32 -0500

    cache: leverages read mutex

--- cache/cache.go
+++ cache/cache.go
@@ -20,8 +20,8 @@ func (c *Cache) Add(key string, value any) {
 }
 
 func (c *Cache) Get(key string) (any, bool) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	return c.cache.Get(lru.Key(key))
 }
 
