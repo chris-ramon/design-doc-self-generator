431bf3556d8d8b4110acca256520392f2ed702e1
Chris Ramón <rchristian.ramon@gmail.com>
2025-07-31 07:35:51 -0500

    cache: wires github.com/golang/groupcache/lru pkg

--- cache/cache.go
+++ cache/cache.go
@@ -1,8 +1,23 @@
 package cache
 
+import "github.com/golang/groupcache/lru"
+
 type Cache struct {
+	cache lru.Cache
+}
+
+func (c *Cache) Add(key string, value any) {
+	c.cache.Add(lru.Key(key), value)
+}
+
+func (c *Cache) Get(key string) (any, bool) {
+	return c.cache.Get(lru.Key(key))
 }
 
 func New() *Cache {
-	return &Cache{}
+	c := &Cache{
+		cache: lru.Cache{},
+	}
+
+	return c
 }
